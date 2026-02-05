21120bbad5e0693777767426cd64954815af1370
pratiktaralkar0079 <pratiktaralkar007@gmail.com>
2026-02-05 13:07:40 +0530

    Add files via upload

---
+++ UNIT1/Q4.py
@@ -0,0 +1,18 @@
+#Creating a Dictionary
+my_dict= {
+     "Name": "ABC",
+     "Age": 25,
+     "City": "Pune"
+}
+print("Original Dictionary:", my_dict)
+
+#Accessing Elements
+print("Name:", my_dict["Name"])
+
+#Adding New Key-Value Pair
+my_dict["Country"]="India"
+print("After adding country:", my_dict)
+
+#Updating Value
+my_dict["Age"]=26
+print("After updating age:", my_dict)
